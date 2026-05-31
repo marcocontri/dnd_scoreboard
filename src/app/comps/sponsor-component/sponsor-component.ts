59d3f55e152c91d462483f8c4ea2764203cb83b5
marcocontri <marcocontri2000@gmail.com>
2026-05-31 11:10:28 +0200

    fixed style and cleared code

---
+++ src/app/comps/sponsor-component/sponsor-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sponsor-component',
+  imports: [],
+  templateUrl: './sponsor-component.html',
+  styleUrl: './sponsor-component.css',
+})
+export class SponsorComponent {
+  
+}
--- src/app/comps/timeout-component/timeout-component.ts
+++ src/app/comps/timeout-component/timeout-component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { SponsorComponent } from "../sponsor-component/sponsor-component";
 
 @Component({
   selector: 'app-timeout-component',
-  imports: [RouterLink],
+  imports: [RouterLink, SponsorComponent],
   templateUrl: './timeout-component.html',
   styleUrl: './timeout-component.css',
 })
